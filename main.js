4982213d055adadbb8c48d0d6a5958fc7d3051de
iamamit-16 <iamakumar1612@gmail.com>
2021-01-28 21:55:02 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function input(value){
+    document.querySelector("#output").value +=value;
+}
+function remove() {
+    document.querySelector('#output').value = '';
+}
+function equal() {
+    document.querySelector('#output').value = eval(document.querySelector('#output').value);
+}
